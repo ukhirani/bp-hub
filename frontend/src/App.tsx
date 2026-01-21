3b2674fe319eb4b779788db2c649deb9497c3237
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-21 23:20:03 +0530

    chore : added initial files to start with

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <div className="min-h-screen"> HOME </div>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage.tsx";
+import Profile from "./pages/Profile";
+import { useState } from "react";
+import Login from "./pages/Login.tsx";
+export default function App() {
+  const [token, setToken] = useState<string | null>(null);
 
-export default App;
+  // show
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+  return (
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
