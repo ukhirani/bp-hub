746befa536e901bec082cf5b7812f7a06702c7a9
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-29 18:33:38 +0530

    refactor : refactored the main.go

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 const Port = ":8080"
@@ -12,23 +13,28 @@ type LoginResponse struct {
 	Token string `json:"token"`
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
+func configureHeader(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Content-Type", "application/json")
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	configureHeader(w)
 
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusOK)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-
 	response := LoginResponse{
-		Token: "test123",
+		Token: "token from backend",
 	}
 
-	json.NewEncoder(w).Encode(response)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		fmt.Println("error encoding response to writer : ", err)
+	}
 }
 
 func main() {
@@ -37,5 +43,6 @@ func main() {
 
 	if err := http.ListenAndServe(Port, nil); err != nil {
 		fmt.Println("Server error:", err)
+		os.Exit(1)
 	}
 }
