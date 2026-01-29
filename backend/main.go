f6fd41932764cbb2027160ab108da105146d6470
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-29 18:08:27 +0530

    chore : matched the actual backend to return teh same thing as the dummy backend

--- backend/main.go
+++ backend/main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 const Port = ":8080"
 
-func server(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "Server started . . . . /n")
-	if err != nil {
-		fmt.Println(err)
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	response := LoginResponse{
+		Token: "test123",
 	}
+
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
-	fmt.Println("Starting server on", Port)
-	http.HandleFunc("/", server)
+	fmt.Println("API is running on http://localhost" + Port)
+	http.HandleFunc("/login", loginHandler)
+
 	if err := http.ListenAndServe(Port, nil); err != nil {
-		fmt.Printf("Error Printing on port %v - %v", Port, err)
+		fmt.Println("Server error:", err)
 	}
 }
