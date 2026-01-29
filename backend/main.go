ba64f553331a51a254d56de64d0b072b0a1b3694
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-29 22:18:21 +0530

    feat : did mongodb atlas connection

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"context"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 const Port = ":8080"
@@ -36,7 +44,31 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// YvbkJF4nRemBdAsE
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	mongoURI := os.Getenv("MONGO_DB_URI")
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Send a ping to confirm a successful connection
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
 	fmt.Println("API is running on http://localhost" + Port)
 	http.HandleFunc("/login", loginHandler)
 
