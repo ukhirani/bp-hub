926c8e11cd5d37735ed6759ad1e2d6902a936de0
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-29 13:28:01 +0530

    chore : just changed the log to print next line

--- backend/main.go
+++ backend/main.go
@@ -8,7 +8,7 @@ import (
 const Port = ":8080"
 
 func server(w http.ResponseWriter, r *http.Request) {
-	_, err := fmt.Fprintf(w, "Server started . . . . ")
+	_, err := fmt.Fprintf(w, "Server started . . . . /n")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -17,5 +17,7 @@ func server(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Starting server on", Port)
 	http.HandleFunc("/", server)
-	http.ListenAndServe(Port, nil)
+	if err := http.ListenAndServe(Port, nil); err != nil {
+		fmt.Printf("Error Printing on port %v - %v", Port, err)
+	}
 }
