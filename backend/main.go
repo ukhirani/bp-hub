d59a8c4f239a59ed0335328ad9a476d207f6fb95
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-29 18:12:52 +0530

    fix : trying to fix the CORS error

--- backend/main.go
+++ backend/main.go
@@ -13,8 +13,16 @@ type LoginResponse struct {
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
 
 	response := LoginResponse{
 		Token: "test123",
