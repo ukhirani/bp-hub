45dce81fc50b04f08fe090b39c3d10935f175614
Umang Hirani <umanghirani.exe@gmail.com>
2026-01-30 17:34:14 +0530

    refactor : moved configure header to the util package

---
+++ backend/utils/configure-header.go
@@ -0,0 +1,10 @@
+package utils
+
+import "net/http"
+
+func ConfigureHeader(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Content-Type", "application/json")
+}
